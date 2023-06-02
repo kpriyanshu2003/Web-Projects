15dc3f3fa64151a81fd04158c452b665875d2426
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-06-02 16:06:01 +0530

    Updated directory and make an Index for projects

--- image-caraousel/script.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  new Splide("#image-carousel").mount();
-});
---
+++ script.js
@@ -0,0 +1,29 @@
+const root = document.getElementById("root");
+
+fetch(`./data.json`)
+  .then((response) => response.json())
+  .then((data) => {
+    // Use the JSON data here
+    console.log(data);
+    let tmp = `<tr>
+        <th>Sl No</th>
+        <th>Name</th>
+        <th>Description</th>
+      </tr>`;
+    for (i in data) {
+      data[i].status
+        ? (tmp =
+            tmp +
+            `<tr>
+          <td>${parseInt(i) + 1}</td>
+          <td><a href="${data[i].link}">${data[i].name}</a></td>
+          <td>${data[i].description}</td>
+          </tr>
+      `)
+        : "";
+    }
+    root.innerHTML = tmp;
+  })
+  .catch((error) => {
+    console.error("Error fetching JSON:", error);
+  });
