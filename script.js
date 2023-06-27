d6e0de4217eca5f9572c846ab77d58b88acc595d
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-06-27 15:34:27 +0530

    Updated listing and logicc

--- script.js
+++ script.js
@@ -9,16 +9,14 @@ fetch(`./data.json`)
         <th>Description</th>
       </tr>`;
     for (i in data) {
-      data[i].status
-        ? (tmp =
-            tmp +
-            `<tr>
+      tmp =
+        tmp +
+        `<tr>
           <td>${parseInt(i) + 1}</td>
           <td><a href="${data[i].link}">${data[i].name}</a></td>
           <td>${data[i].description}</td>
           </tr>
-      `)
-        : "";
+      `;
     }
     root.innerHTML = tmp;
   })
