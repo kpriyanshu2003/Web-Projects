5f403f6493fc1e367a293e2476f8dea46c647981
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-07-13 13:34:32 +0530

    code fix added

--- data/dynamic-code-injection/script.js
+++ data/dynamic-code-injection/script.js
@@ -7,7 +7,7 @@ document.head.innerHTML += `
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
 `;
+
 document.body.innerHTML += `
-        <div style="position:absolute; bottom:10px; font-family: 'Raleway', sans-serif; left: 50%; transform: translateX(-50%);">
-        <span>Made with <span style="color:red">&#10084;</span> by Priyanshu</span>
-        </div>`;
+<div style="width:100%; text-align:center;font-family: 'Raleway', sans-serif;"><span>Made with <span style="color:red">&#10084;</span> by Priyanshu</span></div>
+`;
