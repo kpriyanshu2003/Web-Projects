192e5106cc2c7b6d9b5a862f148a8ae9ffe9a117
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-07-13 12:51:26 +0530

    Updated DYCI

---
+++ data/dynamic-code-injection/README.md
@@ -0,0 +1,7 @@
+# Dynamic Code Injection
+
+It works using the concept of positioning and JS(mainly).
+
+1. Positioning is used to place it anywhere on the screen or with respect to any particular element
+
+2. JS - It is used to add HTML to webpage mainly using window on load event listener.
--- data/dynamic-code-injection/script.js
+++ data/dynamic-code-injection/script.js
@@ -2,12 +2,12 @@
     Adds the above line to any page, this script is added to.
 */
 
-document.head.innerHTML = `
+document.head.innerHTML += `
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
 `;
-document.body.innerHTML = `
+document.body.innerHTML += `
         <div style="position:absolute; bottom:10px; font-family: 'Raleway', sans-serif; left: 50%; transform: translateX(-50%);">
         <span>Made with <span style="color:red">&#10084;</span> by Priyanshu</span>
         </div>`;
